f1cbf061561f24acb59ff71d8d0cc11608c5a96f
SKYBIRDSGP <sgpatki3422@gmail.com>
2024-04-09 23:18:20 +0530

    [Filter-Algorithm]: Updates code

--- Position_Prediction_Algo/filter_algorithm.cpp
+++ Position_Prediction_Algo/filter_algorithm.cpp
@@ -3,38 +3,37 @@
 #include<stdlib.h>
 #include<vector>
 #include<math.h>
-#include<ctime>
-#include <unistd.h> 
+
 
 using namespace std;
 float x_pred, x_est;
-const float α = 0.08;
+const float a = 0.08;
 const float β = 0.002;
 
 int main(){
-    //------------------ α_β_filter_algorithm--------------------------------------------------
+    //------------------ a_β_filter_algorithm--------------------------------------------------
     // STEP0 : Initialization
-    float x̂;      // Estimated Position
-    float x̃ = 0;  // Predicted Position
-    float v̂_prev = 0.0 ;
-    float v̂_curr = 0.0 ;
+    float x_est;      // Estimated Position
+    float x_pred = 0;  // Predicted Position
+    float v_prev = 0.0 ;
+    float v_curr = 0.0 ;
     float x_mes = 50 ;
     float dT = 0.2;
-    float r̂ ;
+    float r ;
     float x = 10;
 
     while(1){
         // STEP1 : State Prediction
-        x̃ = x̂ + dT*v̂_prev ;
-        v̂_curr = v̂_prev ;
+        x_pred = x_est + dT*v_prev ;
+        v_curr = v_prev ;
         // STEP2 : Calculation of a Residual position
-        r̂ = x - x̃;
+        r = x - x_pred;
         // STEP3: Measurement update (correction):
-        x̂ = x̃ + α *r̂;
-        v̂_curr = v̂_prev + β*r̂/dT ;
+        x_est = x_pred + a *r;
+        v_curr = v_prev + β*r/dT ;
  
-        cout<<"The predicted position in the next 0.2 seconds is : "<<x̃<<endl;
-        cout<<"The estimated position in the next 0.2 seconds is : "<<x̂<<endl;
+        cout<<"The predicted position in the next 0.2 seconds is : "<<x_pred<<endl;
+        cout<<"The estimated position in the next 0.2 seconds is : "<<x_est<<endl;
 
 
     }
